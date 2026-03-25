8ea4eb38d74addb2c669c151b9482713065624a5
hwang67120 <hwang67120@gmail.com>
2026-03-25 11:20:54 +0900

    계산기을 다양한 방법으로 만든코딩

--- src/rechner/classe/App.java
+++ src/rechner/classe/App.java
@@ -1,6 +1,7 @@
 package rechner.classe;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -13,61 +14,74 @@ public class App {
 
 
         while (true) {
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                mycal.setNum(scanner.nextInt());
 
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            mycal.setNum(scanner.nextInt());
+                System.out.print("연산자을 입력 하세요;");
+                String op = scanner.next();
+                System.out.print("두 번째 숫자을 입력하세요: ");
+                mycal.setZahl(scanner.nextInt());
 
-            System.out.print("연산자을 입력 하세요;");
-            String op = scanner.next();
 
-            System.out.print("두 번째 숫자을 입력하세요: ");
-            mycal.setZahl(scanner.nextInt());
+                int result = 0;
+                boolean validOperation = true;
 
-            int result = 0;
-            boolean validOperation = true;
 
-            switch (op) {
-                case "+":
-                    result = mycal.getNum() + mycal.getZahl();
-                    break;
-                case "-":
-                    result = mycal.getNum() - mycal.getZahl();
-                    break;
-                case "*":
-                    result = mycal.getNum() * mycal.getZahl();
-                    break;
-                case "/":
-                    if (mycal.getZahl() != 0) {
-                        result = mycal.getNum() / mycal.getZahl();
-                    } else {
-                        System.out.println("0으로 나눌수 없습니다. 다시 입력 하세요.");
+                switch (op) {
+
+                    case "+":
+                        result = mycal.getNum() + mycal.getZahl();
+                        break;
+                    case "-":
+                        result = mycal.getNum() - mycal.getZahl();
+                        break;
+                    case "*":
+                        result = mycal.getNum() * mycal.getZahl();
+                        break;
+                    case "/":
+                        if (mycal.getZahl() != 0) {
+                            result = mycal.getNum() / mycal.getZahl();
+                        } else {
+                            System.out.println("0으로 나눌수 없습니다. 다시 입력 하세요.");
+                            validOperation = false;
+                        }
+
+                        break;
+
+                    default:
+                        System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요");
                         validOperation = false;
-                    }
+                        break;
+                }
 
+                if (!validOperation) {
+                    continue;
+                }
+                System.out.println("계산기 종료(아무 키 입력 시 종료 계속 계산 할 예정이면 y을 입력해주세요.)");
+                String msg = scanner.next();
+                // y 입력시 계속 계산
+                if (!msg.equals("y")) {
+                    System.out.println("계산종료");
                     break;
-
-                default:
-                    System.out.println("유효하지 않은 연산자입니다. 다시 입력하세요");
-                    validOperation = false;
+                } else {
+                    System.out.println("계산 게속");
+                }
+                // 다른 문자열을 넣을시 오류
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("잘못 입력 하셨습니다");
+                System.out.println("계산기 종료(아무 키 입력 시 종료 계속 계산 할 예정이면 y을 입력해주세요.)");
+                String msg = scanner.next();
+                if (!msg.equals("y")) {
                     break;
+                } else {
+                    System.out.println("다시 시작");
+                }
+
             }
 
-            if (!validOperation) {
-                continue;
-            }
-            System.out.println("계산기 종료(아무 키 입력 시 종료 계속 계산 할 예정이면 y을 입력해주세요.)");
-            String msg = scanner.next();
-            if (!msg.equals("y")) {
-                System.out.println("계산종료");
-                break;
-            } else {
-                System.out.println("계산 게속");
-            }
         }
         scanner.close();
-
     }
-
 }
-
-
