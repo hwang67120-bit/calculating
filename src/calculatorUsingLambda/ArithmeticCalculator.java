a4d0df72e1c2e8c1c58a77a5536092d9af6d809e
hwang67120 <hwang67120@gmail.com>
2026-03-26 10:41:08 +0900

    계산기을 다양한 방법으로 만든코딩

--- src/calculatorUsingLambda/ArithmeticCalculator.java
+++
@@ -1,8 +0,0 @@
-package calculatorUsingLambda;
-
-public class ArithmeticCalculator <T extends Number>{
-
-    public double calculate(T x, T y, OperatorType operator) {
-        return operator.Calculate(x, y);
-    }
-}
